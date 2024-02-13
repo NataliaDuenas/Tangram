d55733eef907b4c348fc03e28b374770d0f9f589
NataliaDuenas <104275525+NataliaDuenas@users.noreply.github.com>
2024-02-12 21:37:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,561 +1,212 @@
-let Triangulo1;
-let Triangulo2;
-let Triangulo3;
-let Triangulo4;
-let Triangulo5;
-let Cuandrado1;
-let Cuandrado2;
+let img;
+let display;
+
 let indice;
 let Figuras;
-let fondo;
-let n=0;
-
-function preload() {
-  let i= random(1,6)
-    print('Nivel'+parseInt(i)+'.json')
-    numero='Nivel'+parseInt(i)+'.json'
-  Datos = loadJSON(numero);
+let ColorUsuario = [];
+let TeclaUsuario = "tcla";
+let n = 0;
+let tamaño;
+let areaNiveles;
+let areaLimite;
+let distanciaNiveles;
+let pixeles = 0;
+let Save;
+let Modo;
+let IndiceNiveles = 0;
+let Nivel = [];
+let NumberLevel=3;
+let Datos = [];
+//Display =="home"
+function preload(){
+  let r = random (1,6);
+  let a= parseInt(r)+".png";
+  home= loadImage(a);
+  instructions = loadImage("6.png");
+  cInstructions = loadImage("7.png");
+  sound = loadSound("Sound.mp3");
+  
+  fondoPantalla = loadImage("Fondo.jpg");
+  for (let i = 1; i <= 11; i++) {
+    Nivel.push("Animales" + i + ".json");
   }
+  for (let j = 0; j <= 10; j++) {
+    a = loadJSON(Nivel[j]);
+    Datos.push(a);
+  }
+}
+
+
+
 
 function setup() {
-    createCanvas(windowWidth, windowHeight); 
-  
- Triangulo1  = {
-    x: 0, 
-    y: 0, 
-    xO: 90, 
-    yO: 40, 
-    alpha: 0,
-    xt: 90,
-    yt: 40,
-    permitir: false,
-    tecla: "",
-    rgba: color(3, 150, 54, 255),
-    linea: 1,
-    lineargba: color(0),
-    escala:windowWidth/900,
-    rotar: function (angulo) {
-      if (this.permitir) {
-        this.alpha = angulo;
-      }
-    },
-    desplazar: function (x, y) {
-      if (this.permitir) {
-        this.xO = x;
-        this.yO = y;
-      }
-    },
-    evaluar: function () {
-      a = get(this.xO, this.yO);
-      b = get(mouseX, mouseY);
-      if (arraysIdentical(a, b) || this.tecla == "1") {
-        this.permitir = true;
-      } else {
-        this.permitir = false;
-      }
-    },
-    dibujo: function () {
-      fill(this.rgba);
-      strokeWeight(this.linea);
-      stroke(this.lineargba)
-      push();
-      translate(this.xO, this.yO); // Mueve el canvas segun la funcion dezplazar donde se asigna valor xO y YO
-      rotate(radians(this.alpha)); // Rota el canvas segun la funcion rotar donde se asigna valor a alpha
-      //scale(this.escala)
-      triangle(
-        this.x - 84,
-        this.y - 28,
-        this.x + 84,
-        this.y - 28,
-        this.x,
-        this.y + 56
-      );
-      pop();
-    },
-  };
-
-  Triangulo2 = {
-    x: 0,
-    y: 0,
-    xO: 90,
-    yO: 150,
-    alpha: 0,
-    xt: 90,
-    yt: 150,
-    permitir: false,
-    tecla: "",
-    rgba: color(255, 87, 51, 255),
-    linea: 1,
-    lineargba: color(0),
-    escala:windowWidth/900,
-    rotar: function (angulo) {
-      if (this.permitir) {
-        this.alpha = angulo;
-      }
-    },
-    desplazar: function (xO, yO) {
-      if (this.permitir) {
-        this.xO = xO;
-        this.yO = yO;
-      }
-    },
-    evaluar: function () {
-      a = get(this.xO, this.yO);
-      b = get(mouseX, mouseY);
-      if (arraysIdentical(a, b) || this.tecla == "2") {
-        this.permitir = true;
-      } else {
-        this.permitir = false;
-      }
-    },
-    dibujo: function () {
-      fill(this.rgba);
-      strokeWeight(this.linea);
-      stroke(this.lineargba)
-      push();
-      translate(this.xO, this.yO);
-      rotate(radians(this.alpha));
-      //scale(this.escala)
-      triangle(- 84, - 42, 84, - 42, 0, 42);
-      pop();
-    },
-  };
-
-  Triangulo3 = {
-    x: 0,
-    y: 0,
-    xO: 50,
-    yO: 230,
-    alpha: 0,
-    xt: 50,
-    yt: 230,
-    permitir: false,
-    tecla: "",
-    rgba: color(130, 20, 255, 255),
-    linea: 1,
-    lineargba: color(0),
-    //escala:windowWidth/900,
-    rotar: function (angulo) {
-      if (this.permitir) {
-        this.alpha = angulo;
-      }
-    },
-    desplazar: function (x, y) {
-      if (this.permitir) {
-        this.xO = x;
-        this.yO = y;
-      }
-    },
-    evaluar: function () {
-      a = get(this.xO, this.yO);
-      b = get(mouseX, mouseY);
-      if (arraysIdentical(a, b) || this.tecla == "3") {
-        this.permitir = true;
-      } else {
-        this.permitir = false;
-      }
-    },
-    dibujo: function () {
-      fill(this.rgba);
-      strokeWeight(this.linea);
-      push();
-      translate(this.xO, this.yO);
-      rotate(radians(this.alpha));
-      //scale(this.escala)
-      stroke(this.lineargba)
-      triangle(
-        this.x,
-        this.y + 21,
-        this.x - 42,
-        this.y - 21,
-        this.x + 42,
-        this.y - 21
-      );
-      pop();
-    },
-  };
-
-  Triangulo4 = {
-    x: 0,
-    y: 0,
-    xO: 150,
-    yO: 230,
-    alpha: 0,
-    xt: 150,
-    yt: 230,
-    permitir: false,
-    tecla: "",
-    rgba: color(0, 6, 194, 255),
-    linea: 1,
-    lineargba: color(0),
-    escala:windowWidth/900,
-    rotar: function (angulo) {
-      if (this.permitir) {
-        this.alpha = angulo;
-      }
-    },
-    desplazar: function (x, y) {
-      if (this.permitir) {
-        this.xO = x;
-        this.yO = y;
-      }
-    },
-    evaluar: function () {
-      a = get(this.xO, this.yO);
-      b = get(mouseX, mouseY);
-      if (arraysIdentical(a, b) || this.tecla == "4") {
-        this.permitir = true;
-      } else {
-        this.permitir = false;
-      }
-    },
-    dibujo: function () {
-      fill(this.rgba);
-      strokeWeight(this.linea);
-      stroke(this.lineargba)
-      push();
-      translate(this.xO, this.yO);
-      rotate(radians(this.alpha));
-      //scale(this.escala)
-      triangle(
-        this.x,
-        this.y + 21,
-        this.x - 42,
-        this.y - 21,
-        this.x + 42,
-        this.y - 21
-      );
-      pop();
-    },
-  };
-
-  Triangulo5 = {
-    x: 0,
-    y: 0,
-    xO: 70,
-    yO: 300,
-    alpha: 0,
-    xt: 70,
-    yt: 300,
-    permitir: false,
-    tecla: "",
-    rgba: color(222, 21, 42, 255),
-    linea: 1,
-    lineargba: color(0),
-    escala:windowWidth/900,
-    rotar: function (angulo) {
-      if (this.permitir) {
-        this.alpha = angulo;
-      }
-    },
-    desplazar: function (x, y) {
-      if (this.permitir) {
-        this.xO = x;
-        this.yO = y;
-      }
-    },
-    evaluar: function () {
-      a = get(this.xO, this.yO);
-      b = get(mouseX, mouseY);
-      if (arraysIdentical(a, b) || this.tecla == "5") {
-        this.permitir = true;
-      } else {
-        this.permitir = false;
-      }
-    },
-    dibujo: function () {
-      fill(this.rgba);
-      strokeWeight(this.linea);
-      stroke(this.lineargba)
-      push();
-      translate(this.xO, this.yO);
-      rotate(radians(this.alpha));
-      //scale(this.escala)
-      triangle(
-        this.x + 21,
-        this.y - 63,
-        this.x + 21,
-        this.y + 21,
-        this.x - 63,
-        this.y + 21
-      );
-      pop();
-    },
-  };
-
-  Cuadrado1 = {
-    x: 0,
-    y: 0,
-    xO: 160,
-    yO: 310,
-    alpha: 0,
-    xt: 160,
-    yt: 310,
-    permitir: false,
-    tecla: "",
-    rgba: color(255, 227, 20, 255),
-    linea: 1,
-    lineargba: color(0),
-    rotar: function (angulo) {
-      if (this.permitir) {
-        this.alpha = angulo;
-      }
-    },
-    desplazar: function (x, y) {
-      if (this.permitir) {
-        this.xO = x;
-        this.yO = y;
-      }
-    },
-    evaluar: function () {
-      a = get(this.xO, this.yO);
-      b = get(mouseX, mouseY);
-      if (arraysIdentical(a, b) || this.tecla == "6") {
-        this.permitir = true;
-      } else {
-        this.permitir = false;
-      }
-    },
-    dibujo: function () {
-      fill(this.rgba);
-      strokeWeight(this.linea);
-      stroke(this.lineargba)
-      push();
-      translate(this.xO, this.yO);
-      rotate(radians(this.alpha));
-      quad(
-        this.x + 42,
-        this.y,
-        this.x,
-        this.y - 42,
-        this.x - 42,
-        this.y,
-        this.x,
-        this.y + 42
-      );
-      pop();
-    },
-  };
-
-  Cuadrado2 = {
-    r: -1,
-    teclar: "",
-    x: 0,
-    y: 0,
-    xO: 110,
-    yO: 390,
-    alpha: 0,
-    xt: 90,
-    yt: 390,
-    permitir: false,
-    tecla: "",
-    rgba: color(26, 206, 220, 255),
-    linea: 1,
-    lineargba: color(0),
-    rotar: function (angulo) {
-      if (this.permitir) {
-        this.alpha = angulo;
-      }
-    },
-    desplazar: function (x, y) {
-      if (this.permitir) {
-        this.xO = x;
-        this.yO = y;
-      }
-    },
-    reflejar: function (rx) {
-      if (this.permitir) {
-        this.r = rx;
-      }
-    },
-    evaluar: function () {
-      a = get(this.xO, this.yO);
-      b = get(mouseX, mouseY);
-      if (arraysIdentical(a, b) || this.tecla == "7") {
-        this.permitir = true;
-      } else {
-        this.permitir = false;
-      }
-    },
-    dibujo: function () {
-      fill(this.rgba);
-      strokeWeight(this.linea);
-      stroke(this.lineargba)
-      push();
-      translate(this.xO, this.yO);
-      rotate(radians(this.alpha));
-      quad(
-        this.x - this.r * 21,
-        this.y - 21,
-        this.x + this.r * 63,
-        this.y - 21,
-        this.x + this.r * 21,
-        this.y + 21,
-        this.x - this.r * 63,
-        this.y + 21
-      );
-      pop();
-    },
-  };
-  
-  Figuras=[Triangulo1, Triangulo2, Triangulo3, Triangulo4, Triangulo5, Cuadrado1, Cuadrado2];
-  
- fondo = Object.assign([], Figuras)
-  for (indice in Figuras) {
-    fondo[indice]=Object.assign([],Figuras[indice])  
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1);
+  if (windowWidth < windowHeight) {
+    tamaño = windowWidth;
+    PosicionBotones = [0, tamaño];
+  } else {
+    tamaño = windowHeight;
+    PosicionBotones = [tamaño, 0];
   }
-  Niveles(fondo)
+  Save = createButton("Save");
+  Left = createButton("Left");
+  Right = createButton("Right");
+  Animales = createButton("Animales");
+  Usuario = createButton("Tus Niveles");
+  Pasar = createButton("Pasar");
+ // Barra = createInput();
+ // Barra.position(0, (19 * tamaño) / 20);
+  Pasar.position(
+    PosicionBotones[0] + (4 / 5) * PosicionBotones[1],
+    PosicionBotones[1] + (4 / 5) * PosicionBotones[0]);
+  Save.size(tamaño / 7);
+  Save.position(PosicionBotones[0], PosicionBotones[1]);
+  Left.size(tamaño / 7);
+  Left.position(
+    PosicionBotones[0] + (1 / 4) * PosicionBotones[1],
+    PosicionBotones[1] + (1 / 4) * PosicionBotones[0]
+  );
+  Right.size(tamaño / 7);
+  Right.position(
+    PosicionBotones[0] + (2 / 4) * PosicionBotones[1],
+    PosicionBotones[1] + (2 / 4) * PosicionBotones[0]
+  );
+  Animales.size(tamaño / 4);
+  Animales.position(
+   (1 / 2) * PosicionBotones[0] + (1 / 10) * PosicionBotones[1],
+   (1 / 2) * PosicionBotones[1] + (1 / 10) * PosicionBotones[0]
+  );
+  Usuario.size(tamaño / 4);
+  Usuario.position(
+   (1 / 10) * PosicionBotones[0] + (1 / 2) * PosicionBotones[1],
+   (1 / 10) * PosicionBotones[1] + (1 / 2) * PosicionBotones[0]
+  );
+  let cx, cy;
+  cx = tamaño / 1.1;
+  cy = tamaño / 1.2;
+  Triangulo1 = new Triangulo(cx, tamaño / 5, "Turquoise", "1", 24);
+  Triangulo2 = new Triangulo(cx, tamaño / 2.15, "Orchid", "2", 24);
+  Triangulo3 = new Triangulo(cx, tamaño / 1.55, "LightGreen", "3", 12);
+  Triangulo4 = new Triangulo(cx, cy, "DarkOrange", "4", 17);
+  Triangulo5 = new Triangulo(tamaño / 9, cy, "DarkMagenta", "5", 12);
+  Cuadrado1 = new Cuadrado(tamaño / 1.6, cy, "Goldenrod", "6");
+  Cuadrado2 = new Cuadrilatero(tamaño / 3, cy, "Crimson", "7");
+
+  Figuras = [
+    Triangulo1,
+    Triangulo2,
+    Triangulo3,
+    Triangulo4,
+    Triangulo5,
+    Cuadrado1,
+    Cuadrado2,
+  ];
+  Fondo1 = new Triangulo(0, 0, "Ivory");
+  Fondo2 = new Triangulo(0, 0, "Ivory");
+  Fondo3 = new Triangulo(0, 0, "Ivory");
+  Fondo4 = new Triangulo(0, 0, "Ivory");
+  Fondo5 = new Triangulo(0, 0, "Ivory");
+  Fondo6 = new Cuadrado(0, 0, "Ivory");
+  Fondo7 = new Cuadrilatero(0, 0, "Ivory");
+
+  Fondo = [Fondo1, Fondo2, Fondo3, Fondo4, Fondo5, Fondo6, Fondo7];
+  let url=  "https://raw.githubusercontent.com/NataliaDuenas/Tangram/main/Niveles/Nivel" +
+        NumberLevel +
+        ".json"
+  loadJSON(url, Data)
+  
+   Animales.mousePressed(Modo3);
+   Usuario.mousePressed(Modo4);
+   Pasar.mousePressed(FastPass);
+  
+  //display
+  
+  SoundButton=createButton ("Sound On");
+  SoundButton.mousePressed (playSong);
+  SoundButton.position(0,0);
+  display="home";
+  if(display=="home"||"instructions"||"cModeIns"){
+    Save.hide();
+    Left.hide();
+    Right.hide();
+    Animales.hide();
+    Usuario.hide();
+    Pasar.hide();
+  //  Barra.hide();
+  }
 }
 
-function Niveles(fondo){
-  let x, y, a, b;  
-  for(indice in fondo ){  
-    x = Datos.x[indice];
-    y= Datos.y[indice]
-    a= Datos.rotacion[indice]
-    fondo[indice].linea=5
-    fondo[indice].lineargba= color(255)
-    fondo[indice].permitir=true
-    fondo[indice].rgba= color(255)
-    fondo[indice].desplazar(x,y)
-    fondo[indice].rotar(a)
-    if (indice==6){
-      fondo[indice].r=Datos.reflejar
-    }
-        fondo[indice].permitir=false
+function playSong (){
+  if(!sound.isPlaying()){
+    sound.play();
+    sound.setVolume(0.1);
+    SoundButton.html("Sound off")
+  }else{
+    sound.pause();
+    SoundButton.html("Sound on")
   }
   
 }
+let bP1=new ButtonPlay(90,230,130,55);
+let bC1=new ButtonCreateIns(290,230,130,55);
+let bC2=new ButtonCreate(305,450,80,28);
+let bR1=new ButtonReturn(120,450,80,28);
+let bI1=new ButtonIns(412,450,80,30);
+let bP2=new ButtonPlay(120,450,80,28);
+let bC3=new ButtonCreateIns(307,450,80,28);
 
- function exportar(Figuras){
-  let x=[]
-  let y=[]
-  let tetha=[]
-  let r=Figuras[6].r
-       for(indice in Figuras){
-         x.push(Figuras[indice].xO)
-         y.push(Figuras[indice].yO)
-         tetha.push(Figuras[indice].alpha)
-       }
-   return {
-       x: x,
-       y: y,
-       rotacion: tetha,
-       reflejar: r,}
-     }
 
 function draw() {
-  background(220);
   
-  for (indice in Figuras) {
-    fondo[indice].dibujo()   
-  }
-  for (indice in Figuras) {
-  Figuras[indice].dibujo()
-  }
-  
-  
-  Teclado(); 
+  if (display=="home"){
+      image (home,0,0,500,500)   
+  bP1.displayButton();
+  bC1.displayButton();
+  bI1.displayButton();
 }
+  if (display=="instructions"){
+    image (instructions,0,0,500,500)
+    bP2.displayButton();
+    bC3.displayButton();
+  }
+  if (display=="game"){
+    if (Modo == 1){
+      JuegosOpciones();
+    }
+    else if (Modo == 3){
+      ModoJuegoPredeterminado();
+      if (IndiceNiveles == 10) {
+      Ganaste();
+      }
+    }
+    else if (Modo == 4){
+      ModoJuegoUsuario();
+      if (PasarNivel()){Ganaste();}
+  }
+ 
+    }
+   if (display=="cModeIns"){
+    image (cInstructions,0,0,500,500);
+    bC2.displayButton();
+    bR1.displayButton();
+  }
+  if (display=="cMode"){
+    ModoCreador();
+  if(SalvarTexto()){Guardable()}else{FigurasCuadrado()}
+}
+  }
 
-function Teclado() {
-for (indice in Figuras) {
-
-  if (Triangulo1.tecla-1 == indice) {
-      if (keyIsDown(LEFT_ARROW))  {
-    Figuras[indice].xO -= 2;}
-  if (keyIsDown(RIGHT_ARROW)) {
-    Figuras[indice].xO += 2;}
-  if (keyIsDown(UP_ARROW))    {
-    Figuras[indice].yO -= 2;}
-  if (keyIsDown(DOWN_ARROW))  {
-    Figuras[indice].yO += 2;}
-  if (keyIsDown(CONTROL))     {
-    Figuras[indice].alpha += 0.5;} 
-  else if (keyIsDown(SHIFT))  {
-     Figuras[indice].alpha -= 0.5;}
+ 
   
-    Figuras[indice].rgba = color(70, 90, 110, 255);
-    Figuras[indice].linea = 5;
-    Figuras[indice].desplazar(Figuras[indice].xO, Figuras[indice].yO); 
-    Figuras[indice].rotar(Figuras[indice].alpha); 
-  }
-  
-    
-  
-}
-}
-function keyReleased() { 
-  if (Cuadrado2.teclar=='r') {
-    Cuadrado2.tecla='7'
-    Cuadrado2.reflejar(-Cuadrado2.r)
-    }}
-function keyPressed() { 
-  for (indice in Figuras){
-  Figuras[indice].evaluar();
-  Cuadrado2.teclar = key;
-  }
-}
-function keyTyped() { 
-  if (key==='s'){ 
-    n+=1
-    saveJSON (exportar(Figuras), 'Nivel'+n)
-  }
-   for (indice in Figuras){
-  Figuras[indice].tecla = key;
-  Figuras[indice].linea = 1;
-  Triangulo1.rgba = color(3, 150, 54, 255);
-  Triangulo2.rgba = color(255, 87, 51, 255);
-  Triangulo3.rgba = color(130, 20, 255, 255);
-  Triangulo4.rgba = color(0, 6, 194, 255);
-  Triangulo5.rgba = color(222, 21, 42, 255);
-  Cuadrado1.rgba = color(255, 227, 20, 255);
-  Cuadrado2.rgba = color(26, 206, 220, 255);}
-}
 
-function mouseWheel(event) {
-  for (indice in Figuras){
-  Figuras[indice].evaluar();
-  A=Figuras[indice].alpha+event.deltaY * 0.5
-  Figuras[indice].rotar(A); }
-} 
-function mouseDragged() {for (indice in Figuras){
-  Figuras[indice].desplazar(mouseX, mouseY);}
-}
-function mousePressed() {
-  for (indice in Figuras){
-  Figuras[indice].evaluar();}
-}
-
-function doubleClicked() {
-  Cuadrado2.reflejar(-Cuadrado2.r);
-}
 
 function arraysIdentical(a, b) {
+  //Autor:Tim Down,https://stackoverflow.com/questions/51276005/javascript-how-to-check-for-certain-items-in-an-array
   var i = a.length;
   if (i != b.length) return false;
   while (i--) {
     if (a[i] !== b[i]) return false;
   }
   return true;
-}
-function dibujo() {
-  strokeWeight(5);
-  rect(0, 0, 168);
-  noStroke();
-  fill(3, 150, 54);
-  triangle(0, 0, 168, 0, 84, 84);
-  fill(255, 87, 51);
-  triangle(0, 0, 0, 168, 84, 84);
-  fill(88, 24, 69);
-  triangle(168, 0, 168, 84, 126, 42);
-  fill(255, 252, 32);
-  quad(168, 84, 126, 42, 84, 84, 126, 126);
-  fill(16, 26, 187);
-  triangle(84, 84, 126, 126, 42, 126);
-  fill(16, 135, 187);
-  quad(42, 126, 126, 126, 84, 168, 0, 168);
-  fill(199, 0, 57);
-  triangle(168, 84, 84, 168, 168, 168);
-  noLoop();
 }
